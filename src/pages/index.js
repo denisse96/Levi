b1d4420c51ba396f36efeca2b3386548fbeabe88
denisse96 <denissedam@gmail.com>
2020-07-29 21:00:12 -0500

    Makey Makey

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,88 @@
-import { Box, Button, Grommet, Heading, } from "grommet"
+import { Achievement, Info } from "grommet-icons"
+import { Anchor, Box, Button, Grommet, Heading, Image } from "grommet"
 import React, { useState } from "react"
 
-import { Achievement } from "grommet-icons"
-import Image from "../components/image"
+import Confetti from 'react-confetti'
+//import Image from "../components/image"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 import Typist from 'react-typist';
 
 const IndexPage = () => {
   const [score, setScore] = useState(0)
-  const [mision1, setMision1] = useState(false)
+  const [mision1, setMision1] = useState(true)
+  const [instmision1, setinstMision1] = useState(false)
+  const [hints, setHints] = useState(false)
+  const [hints2, setHints2] = useState(false)
+  const [makey, setMakey] = useState(false)
+
   const [mision2, setMision2] = useState(false)
   const [sure, setSure] = useState(false)
+  let labelmision1 = "¡Ayúdame a poder comerme la dona!"
+  if (!mision1) {
+    labelmision1 = "¡Ayúdame a cambiar de vestuario!"
+  }
 
 
   return (
     <Grommet>
-      <Box style={{ maxWidth: '1280px' }} margin={{ horizontal: 'auto' }} >
-        <Box margin="auto" direction="column" alignContent="center" gap="large">
+      <Box margin={{ horizontal: 'auto' }} >
+        <Box margin="auto" direction="column" alignContent="center" background="f7f6f6" gap="large">
           <Typist cursor={{ show: "true", hideWhenDone: "true", element: '' }}>
-            <Heading color="purple" level={1} textAlign="center">
+            <Heading color="#f42926" level={1} textAlign="center">
               ¡Hola Levi!
           </Heading>
-            <Heading color="purple" level={1} textAlign="center">
-              Soy Bob el austronauta
+            <Heading color="#ff5756" level={1} textAlign="center">
+              Soy Pulpi el Pulpo
           </Heading>
             <Heading color="lightRed" level={3} textAlign="center">
-              completa la siguiente misión en scratch para poder ganar 1000 puntos
-          </Heading>
+              {labelmision1}
+            </Heading>
           </Typist>
-          <Image />
-          <Button size="small" onClick={() => setMision1(true)} label="Misión 1" primary target="_blank" href="https://csfirst.withgoogle.com/c/cs-first/en/an-unusual-discovery/an-unusual-discovery/an-unusual-discovery.html" ></Button>
-          {mision1 && <Button size="small" label="finished" primary color="orange" onClick={() => setSure(true)}></Button>}
-          {sure && <Box animation="slideRight" direction="column" gap="large">
-            <Heading>¿Estás seguro que acabaste?</Heading>
-            <Button size="small" label="Sí" primary onClick={() => setScore(score + 1000)}></Button>
-            <Button size="small" label="No" primary onClick={() => setScore(score - 1000)}></Button>
-            {score < 0 && <Heading level={4} textAlign="center" margin="auto" > ¡Vuelve a intenterlo!</Heading>}
-            {score > 0 &&
-              <Box direction="column" gap="large" >
-                <Button size="small" label="Misión 2" onClick={() => {
-                  setMision2(true)
-                  setScore(score + 1000)
-                }} target="_blank" primary color="orange" href="https://csfirst.withgoogle.com/c/cs-first/en/an-unusual-discovery/an-unusual-discovery/extensions/code-a-color-change.html" ></Button>
-                {mision2 && <Button size="large" label="¡Felicidades!" target="_blank" href="https://firebasestorage.googleapis.com/v0/b/ferropartes-d4a43.appspot.com/o/Levi.pdf?alt=media&token=94f70470-81ad-4bc8-8830-a9625a16737a" icon={<Achievement></Achievement>}></Button>}
-              </Box>}
-          </Box>}
 
+          <Box height="medium" margin={{ horizontal: 'auto' }} width="medium" background={"url('https://firebasestorage.googleapis.com/v0/b/ferropartes-d4a43.appspot.com/o/Picture1.png?alt=media&token=885392e8-9f21-47fb-82d6-da4fb804b701')"}></Box>
+          {mision1 &&
+            <>
+              <Button primary label="Leer Misión 1 " onClick={() => setinstMision1(true)} />
+              {instmision1 && <Box animation="slideRight">
+                <Heading level={3}>1. Abre scratch</Heading>
+                <Heading level={3}>2. Agrega un fondo</Heading>
+                <Heading level={3}>3. Agrega un sprite</Heading>
+                <Heading level={3}>4. Agrega que el sprite se mueva cada vez que das space</Heading>
+                <Heading level={3}>5. Agrega un segundo sprite para que el primero se destruya al segundo </Heading>
+                <Box direction="row" gap="medium">
+                  <Button primary href="https://scratch.mit.edu/projects/editor/?tutorial=getStarted" label="Abrir Scratch" target="_blank">
+                  </Button>
+                  <Button label="Pista 1" onClick={() => setHints(!hints)} icon={<Info></Info>}></Button>
+                  <Button label="Pista 2" onClick={() => setHints2(!hints2)} icon={<Info></Info>}></Button>
+                  <Button primary label="¡Listo!" onClick={() => {
+                    setMakey(!makey)
+                    setScore((score) => (score + 10000))
+                  }} icon={<Achievement />}></Button>
+
+                </Box>
+
+              </Box>
+              }
+            </>}
+          {hints &&
+            <Image width="medium" src="https://firebasestorage.googleapis.com/v0/b/ferropartes-d4a43.appspot.com/o/Screen%20Shot%202020-07-29%20at%2020.20.26.png?alt=media&token=fce7d394-fe9e-48ef-a7b2-2cd0eefb47f6"></Image>
+          }
+          {hints2 &&
+            <Image width="medium"
+              src="https://firebasestorage.googleapis.com/v0/b/ferropartes-d4a43.appspot.com/o/Screen%20Shot%202020-07-29%20at%2020.34.48.png?alt=media&token=3ae0c3aa-b321-4098-b7f8-c1b30e5b170d"></Image>
+
+          }
+          {makey &&
+            <Box direction="column">
+              <Heading level={3}>6. Agrega Makey Makey a Scratch</Heading>
+              <Image height="800px" src="https://firebasestorage.googleapis.com/v0/b/ferropartes-d4a43.appspot.com/o/Screen%20Shot%202020-07-29%20at%2020.51.01.png?alt=media&token=e6207767-2cb8-4506-a9c0-2f7d6819405a"></Image>
+              <Heading level={3}>7. Conecta a computadora y a banana</Heading>
+            </Box>}
 
           <Box animation={{ type: "pulse", delay: "8000" }}>
             <Heading textAlign="center">
-              Puntos actuales {score}
+              Puntos  {score}
             </Heading>
           </Box>
         </Box>
